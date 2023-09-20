c109f4c880da30baeb721645ec80bf2ce8a35f57
caozongchao <caozongchao@163.com>
2023-09-20 11:40:58 +0800

    Update SiteController.php

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -83,7 +83,7 @@ class SiteController extends BaseController
     {
         if ($this->xianlu == 1) {
             $func = 'agent1';
-            $preAddress = 'json.txt';
+            $preAddress = 'yuncaidan.txt';
         }elseif($this->xianlu == 2){
             $func = 'agent2';
             $preAddress = 'list';
